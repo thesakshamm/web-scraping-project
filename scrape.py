90203cc68db3c5a720d23f4b8bbc16a3cf270338
Saksham Shrestha <sakshamstha5514@gmail.com>
2026-01-23 09:18:50 +0545

    git push

--- scrape.py
+++ scrape.py
@@ -13,14 +13,12 @@ response = requests.get(url)
 # data = response.json()
 
 
-# Column headers
+# csv write
 headers = ["Symbol", "LTP", "Percentage Change", "Volume"]
-
-# Data rows
 data_to_dump = response.json()
 
-# Writing to a CSV file
+
 with open("data.csv", "w", newline="") as f:
     writer = csv.writer(f)
-    writer.writerow(headers)  # Write header
-    writer.writerows(data_to_dump)  # Write data rows
+    writer.writerow(headers)
+    writer.writerows(data_to_dump)
